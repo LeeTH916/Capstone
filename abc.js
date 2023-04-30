99f4750c9638ee4861db9323421427bbe66006aa
LeeTH916 <118820838+LeeTH916@users.noreply.github.com>
2023-04-30 15:46:54 +0900

    Update abc.js

--- abc.js
+++ abc.js
@@ -2,6 +2,7 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.120/build/three.mod
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.120/examples/jsm/controls/OrbitControls.js';
 import { OBJLoader } from "https://cdn.jsdelivr.net/npm/three@0.120/examples/jsm/loaders/OBJLoader.js";
 
+
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
